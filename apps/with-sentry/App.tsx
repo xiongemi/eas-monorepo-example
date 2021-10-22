e75d5b030502a060b9ad59a55371baa667d50459
Cedric van Putten <me@bycedric.com>
2021-10-22 14:20:25 +0200

    chore(apps/sentry): reinitialize the sentry example

--- apps/with-sentry/App.tsx
+++ apps/with-sentry/App.tsx
@@ -1,24 +1,11 @@
-import { Paragraph, Strong } from "@acme/ui";
-import { StatusBar } from "expo-status-bar";
-import { useCallback } from "react";
-import { Button, StyleSheet, View } from "react-native";
-import * as Sentry from "sentry-expo";
-
-Sentry.init({
-  dsn: "https://2034b43a8da54401be835e15d1031bb9@o58562.ingest.sentry.io/5878591",
-});
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const onTriggerError = useCallback(() => {
-    throw new Error("This is an error");
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Paragraph>
-        Hello from an <Strong>EAS</Strong> monorepo (with-sentry)
-      </Paragraph>
-      <Button title="Trigger error" onPress={onTriggerError} />
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });

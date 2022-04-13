8fa37f570ea0e743ebe26dab766e439d8241a955
Emily Xiong <xiongemi@gmail.com>
2022-04-13 15:10:34 -0400

    migrate test and lint to nx

---
+++ packages/babel/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'babel',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/babel',
+};
